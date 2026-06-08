d30579da3fe8d1d59609f59b06241201dcd2f2a3
changoloco066 <bigoteschicos@gmail.com>
2026-06-08 00:02:25 -0600

    estructura inicial

--- DukeQuill/src/main/java/com/dukequill/dictionary/Dictionary.java
+++ DukeQuill/src/main/java/com/dukequill/dictionary/Dictionary.java
@@ -1,5 +1,25 @@
 package com.dukequill.dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
 public class Dictionary {
-    
+
+    private Set<String> words = new HashSet<>();
+
+    public void loadDictionary() {
+       BufferedReader br = new BufferedReader(new FileReader(DukeQuill/src/resources/dictionary/Spanish.dic));
+       String line;
+
+       while((line = br.readLine()) != null){
+
+       }
+       br.close();
+    }
+
+    public boolean contains(String word) {
+        return words.contains(word);
+        
+    }
 }

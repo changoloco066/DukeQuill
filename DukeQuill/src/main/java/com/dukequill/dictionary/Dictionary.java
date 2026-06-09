7a9f31af696616469db1f7c67476e05b07433a5f
changoloco066 <bigoteschicos@gmail.com>
2026-06-08 23:03:29 -0600

    dictionary v1

--- DukeQuill/src/main/java/com/dukequill/dictionary/Dictionary.java
+++ DukeQuill/src/main/java/com/dukequill/dictionary/Dictionary.java
@@ -1,7 +1,15 @@
-package com.dukequill.dictionary;
+/*
 
+words - variable asignada para la coleccion del diccionario completo
+word - variable asignada para la palabra que recien se acaba de leer 
+
+*/
+
+package com.dukequill.dictionary;
+import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Dictionary {
@@ -9,13 +17,25 @@ public class Dictionary {
     private Set<String> words = new HashSet<>();
 
     public void loadDictionary() {
-       BufferedReader br = new BufferedReader(new FileReader(DukeQuill/src/resources/dictionary/Spanish.dic));
-       String line;
+        boolean firstLine = true;
+        try(BufferedReader br = new BufferedReader(new FileReader("DukeQuill/src/resources/dictionary/Spanish.dic"))){
+        String line;
 
-       while((line = br.readLine()) != null){
+        while((line = br.readLine()) != null){
+            if(firstLine){
+                firstLine = false;
+                continue;
+            } 
+            String word = line.split("/")[0];
+            words.add(word);
+        }
 
-       }
-       br.close();
+        }catch(FileNotFoundException e){
+            System.out.println("No se encontro el archivo");
+        }catch(IOException e){
+            System.out.println("Algo salio mal :(");
+        }
+        
     }
 
     public boolean contains(String word) {

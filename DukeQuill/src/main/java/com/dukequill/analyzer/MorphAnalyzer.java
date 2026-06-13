a38e4c207a2967ea437c3f1952491ad43b4b7706
changoloco066 <bigoteschicos@gmail.com>
2026-06-12 21:59:54 -0600

    creacion de la clase Mprph analyzer

---
+++ DukeQuill/src/main/java/com/dukequill/analyzer/MorphAnalyzer.java
@@ -0,0 +1,18 @@
+package com.dukequill.analyzer;
+
+import com.dukequill.*;
+import com.dukequill.analyzer.SpellChecker;
+import com.dukequill.analyzer.SpellErrors;
+import com.dukequill.lexer.Token;
+import com.dukequill.dictionary.Dictionary;
+import com.dukequill.lexer.Lexer;
+
+
+public class MorphAnalyzer {
+
+    public boolean isValidWord(String word) {
+        // aquí va la lógica de LanguageTool
+        return false;
+    }
+}
+

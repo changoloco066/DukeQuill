d37755d884decf90a96de4794076208ba417de8c
changoloco066 <bigoteschicos@gmail.com>
2026-06-13 21:01:56 -0600

    morph analyzer

--- DukeQuill/src/main/java/com/dukequill/analyzer/MorphAnalyzer.java
+++ DukeQuill/src/main/java/com/dukequill/analyzer/MorphAnalyzer.java
@@ -7,12 +7,21 @@ import com.dukequill.lexer.Token;
 import com.dukequill.dictionary.Dictionary;
 import com.dukequill.lexer.Lexer;
 
+import org.languagetool.language.Spanish;
+import org.languagetool.tagging.Tagger;
+import java.util.List;
 
 public class MorphAnalyzer {
 
-    public boolean isValidWord(String word) {
-        // aquí va la lógica de LanguageTool
-        return false;
+    private final Tagger tagger;
+
+    public MorphAnalyzer() throws Exception{
+        tagger = new Spanish().createDefaultTagger();
+    }
+
+    public boolean isValidWord(String word) throws Exception{
+        List<?> tags = tagger.tag(List.of(word));
+        return !tags.isEmpty();
     }
 }
 

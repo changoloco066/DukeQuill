993a71e8c0a4e9681cea527a4b6560a9cf8596df
changoloco066 <bigoteschicos@gmail.com>
2026-06-22 22:00:34 -0600

    errores ortograficos

--- DukeQuill/src/main/java/com/dukequill/rules/UpperCaseRule.java
+++ DukeQuill/src/main/java/com/dukequill/rules/UpperCaseRule.java
@@ -8,7 +8,7 @@ import com.dukequill.lexer.TokenType;
 public class UpperCaseRule implements Rule {
     @Override
     public String getRuleName() {
-        return "Mayuscula";
+        return "Mayúscula";
     }
 
     @Override
@@ -21,7 +21,7 @@ public class UpperCaseRule implements Rule {
             Token despues = tokens.get(i + 2);
 
             if(actual.getType() == TokenType.SINGLE_PUNCTUATION_SIGN && actual.getLexeme().equals(".") && siguiente.getType() == TokenType.SPACES && despues.getType() == TokenType.WORD && !Character.isUpperCase(despues.getLexeme().charAt(0))){
-                violations.add(new RuleViolation(actual, "Falta una mayuscula despues del punto ' " + actual.getLexeme() + " '", "Mayuscula"));
+                violations.add(new RuleViolation(actual, "Falta una mayúscula después del punto ' " + actual.getLexeme() + " '", "Mayúscula"));
             }
         }
        return violations;

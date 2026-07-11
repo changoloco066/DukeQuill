353a751c702fdaf0abada66e4456a8299fa26c71
changoloco066 <bigoteschicos@gmail.com>
2026-07-10 18:04:48 -0600

    feat: integrar FlatLaf en la interfaz

--- DukeQuill/src/main/java/com/dukequill/Main.java
+++ DukeQuill/src/main/java/com/dukequill/Main.java
@@ -1,15 +1,16 @@
 package com.dukequill;
 
 import javax.swing.UIManager;
-
 import com.dukequill.gui.MainWindow;
+import com.formdev.flatlaf.*;
 
 public class Main {
     public static void main (String[]args) throws Exception{
         System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
         System.setProperty("file.encoding", "UTF-8");
+        System.setProperty("flatlaf.useWindowDecorations", "false");
             
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        FlatLightLaf.setup();        
         MainWindow gui = new MainWindow();
         javax.swing.SwingUtilities.invokeLater(() -> {
             gui.setVisible(true);
